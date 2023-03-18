fc45304482b378f5c754161f82bf44793da5a0fd
lukas <lukasglomb01@gmail.com>
2023-03-18 20:47:25 +0100

    Testing

--- src/component/otto.js
+++ src/component/otto.js
@@ -1,23 +1,19 @@
-import { useEffect, useState } from 'react';
 import Nightmare from 'nightmare';
 import electron from 'electron';
+const nightmare = require('nightmare')()
 
 export async function checkPrice() {
-    const nightmare = Nightmare({
-        show: false,
-        electronPath: electron,
-        waitTimeout: 5000,
-    });
 
-    const priceString = await nightmare
-        .goto('https://www.otto.de/p/swing-harmonie-sitzgruppe-poly-rattan-sitzgarnitur-sydney-esstisch-lounge-gartenmoebel-garten-garnitur-set-1x-tisch-plus-2x-stuehle-plus-1x-sitzbank-plus-2x-hocker-S0Q3V0AW/#variationId=S0Q3V0AWKQHX')
-        .wait('span.pl_headline300')
-        .evaluate(() => document.querySelector('span.pl_headline300').innerText)
-        .end();
+    const priceString = await nightmare.goto("https://www.otto.de/p/steadyrack-fahrradhalter-steadyrack-fahrrad-wandhalter-mtb-road-rack-S0Z0J03H/#variationId=S0Z0J03HZMMQ")
+        .wait("span.pl_headline300")
+        .evaluate(() => document.querySelector("span.pl_headline300").innerText)
+        .end()
 
-    const priceNumber = parseFloat(priceString.replace('€', '').replace(',', '.'));
+    const priceNumber = parseFloat(priceString.replace('€', '').replace(',', '.'))
 
-    console.log(priceNumber);
+    console.log(priceNumber)
 
-    return priceNumber;
+    return priceNumber
 }
+
+
